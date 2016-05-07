048db2de46c1968ac22c4060b38f32ebb57f3bb4
Tom Futureworkz <tom@futureworkz.com>
2016-05-07 10:29:28 +0700

    Add to_currency helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def to_currency(amount, unit: '$')
+    unit = "#{unit}" if unit.present?
+    number_to_currency(amount, :unit => unit, :separator => ".", :delimiter => ",", format: "%u%n")
+  end
 end
