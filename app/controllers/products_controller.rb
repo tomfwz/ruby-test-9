1f2737fac65beff4f7549d474d8136486f3c8465
Tom Futureworkz <tom@futureworkz.com>
2016-05-07 11:05:02 +0700

    WIP: Add ProductFilter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    @product_filter = ProductFilter.new(product_filter_params)
     @products = Product.all
+  end
+
+  private
+
+  def product_filter_params
+    return nil if params[:product_filter].nil?
+    params.require(:product_filter).permit(:min_price, :max_price, brand_ids: [])
   end
 end
---
+++ app/services/product_filter.rb
@@ -0,0 +1,26 @@
+class ProductFilter
+  include ActiveModel::Conversion
+  extend  ActiveModel::Naming
+
+  attr_accessor :brand_ids, :min_price, :max_price
+
+
+  def initialize(options)
+    return if options.nil?
+    @brand_ids = options[:brand_ids]
+    @min_price = options[:min_price].to_f
+    @max_price = options[:max_price].to_f
+  end
+
+  def result
+    result = Product.all
+    result = result.find_by_brand_ids(@brand_ids)   if @brand_ids.present?
+    result = result.where('price >= ?', @min_price) if @min_price.present?
+    result = result.where('price <= ?', @max_price) if @max_price.present?
+    result
+  end
+
+  def persisted?
+    false
+  end
+end
